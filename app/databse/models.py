8b0795df7b83630f1991f8713996d8665d7a836f
YelibeT <yelibe44@gmail.com>
2026-08-12 10:59:43 +0300

    fixed bot integration issues

---
+++ app/databse/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+from app.databse.db import Base
+
+class User(Base):
+    __tablename__="users"
+    id=Column(Integer, primary_key=True, index=True)
+    telegram_id=Column(Integer, unique=True)
+    leetcode_username=Column(String, nullable=False)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from app.routers.users import users_router
+from app.databse.db import engine, Base
+from app.databse import models
 
+Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="LeetTracker API")
+app = FastAPI()
 app.include_router(users_router)
 
 @app.get("/")
